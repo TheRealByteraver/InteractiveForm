15eedc295aff7d80cafd539c802098f26bd93f65
Erland <erland.vo@telenet.be>
2021-05-07 11:32:32 +0200

    step 8 done, no warning messages for now. Bugfix in activitiesBox.addEventListener

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const otherJobRoleInput = document.getElementById('other-job-role');
 const userTitleSelect = document.getElementById('title');
 const colorSelect = document.getElementById('color');
 const designSelect = document.getElementById('design');
+const paymentSelect = document.getElementById('payment');
 
 // start by giving focus to the first (top left) input on the form
 nameInput.focus();
@@ -38,7 +39,7 @@ colorSelect.setAttribute('disabled', true);
     Steel Blue      (I love JS shirt only)
     Dim Grey        (I love JS shirt only)
 
-    The function removeColorAvailabilityInfo removes the extra 
+    The function removeColorAvailabilityInfo strips the extra 
     availability information such as '(JS Puns shirt only)' and
     leaves only the color itself (e.g. 'Cornflower Blue').
 */
@@ -90,39 +91,68 @@ designSelect.addEventListener('change', (event) => {
 });
 
 // step 6
-
 // select the fieldset containing all the activity checkboxes
 const activitiesBox = document.getElementById('activities');
 
-activitiesBox.addEventListener('change', (event) => {
-    if(event.target.tagName='input') {
-        let totalCost = 0;
-        const inputs = activitiesBox.querySelectorAll('input');
-        for(let i = 0; i < inputs.length; i++) {
-            if(inputs[i].checked) {
-                totalCost += parseInt(inputs[i].dataset.cost);
-            }
+// This function calculates the total cost based on the checked courses
+function getTotalCost() {
+    let totalCost = 0;
+    const inputs = activitiesBox.querySelectorAll('input');
+    for(let i = 0; i < inputs.length; i++) {
+        if(inputs[i].checked) {
+            totalCost += parseInt(inputs[i].dataset.cost);
         }
-        const activitiesCost = activitiesBox.querySelector('#activities-cost');
+    }
+    return totalCost;
+}
+
+// The following event listener recalculates and displays the total cost each
+// time a checkbox is checked or unchecked
+activitiesBox.addEventListener('change', (event) => {    
+    if(event.target.tagName === 'INPUT') {
+        let totalCost = getTotalCost();
+        const activitiesCost = document.getElementById('activities-cost');
         activitiesCost.innerText = `Total: $${totalCost}`;
     }
 });
 
 // step 7
+// The array 'paymentOptions' holds a list of the payment options id's as 
+// specified in the html. We use the fact that these exact values are also 
+// present in the 'value' attribute of the <select> list with the payment 
+// options.
+function getPaymentIdsArray() {
+    const paymentSelectOptions = paymentSelect.children;
+    const paymentIds = [];
+    // populate the paymentOptions list based on the <select> list in the html
+    for(let i = 0; i < paymentSelectOptions.length; i++) {
+        paymentIds.push(paymentSelectOptions[i].value);
+    }
+    return paymentIds;
+};
+const paymentOptions = getPaymentIdsArray();
+
 // set the credit card option as the default
 const creditCardOption = payment.querySelector('option[value="credit-card"]');
 creditCardOption.setAttribute('selected', true);
 
+// The function getPaymentMethodId() returns the id of the currently
+// active payment method 
+function getPaymentMethodId() {
+    const index = document.getElementById('payment').selectedIndex;
+    return paymentOptions[index];
+}
+
 // The function selectPaymentMethod hides all payment method input & info 
 // fields except for the one that has an id provided by the argument
 function selectPaymentMethod(paymentId) {
-    // hide all fields
-    document.getElementById('credit-card').style.display = 'none';
-    document.getElementById('paypal').style.display = 'none';
-    document.getElementById('bitcoin').style.display = 'none';
-
+    // hide all fields. We skip the first payment Option as it is not
+    // a real payment option
+    for( let i = 1; i < paymentOptions.length; i++ ) {
+        document.getElementById(paymentOptions[i]).style.display = 'none';
+    }
     // show selected field again
-    document.getElementById(paymentId).style.display = '';   
+    document.getElementById(getPaymentMethodId()).style.display = '';   
 }
 
 // Listen for payment method changes
@@ -132,3 +162,98 @@ payment.addEventListener('change', (event) => {
 
 // By default, we select the credit card payment method
 selectPaymentMethod('credit-card');
+
+// step 8
+const form = document.querySelector('form');
+
+// The form submit event listener does a full validation of the input fields
+// and prevents submission if some information is wrong or missing
+form.addEventListener('submit', (event) => { // correct event == submit !!! DEBUG!!
+
+    /*
+      **Note**: these little helper functions check if the information was
+      entered correctly in the form input fields. They return true on success
+      or false if there is missing/ wrong information
+    */
+
+    // A name should at least contain two characters, excluding spaces
+    function isValidName() {
+        const name = document.getElementById('name').value;
+        return (name.trim().length >= 2);
+    }
+    // Only addresses with a .com top level domain are accepted (as instructed)
+    // The following email addresses will be considered valid by this function:
+    // steve.clarke.2@gmail.com, john@team.treehouse.com, peter@a.com
+    function isValidEmail() {
+        const email = document.getElementById('email').value;
+        return /^(\w+\.?){1,3}@(\w+\.?){1,3}\.com$/.test(email);
+    }
+
+    // The "Register for Activities" section must have at least one 
+    // activity selected. This means the total cost can't be zero
+    function AreActivitiesSelected() {
+        return getTotalCost() > 0;
+    }
+
+   // The isValidCreditCard function checks if all the credit card information
+   // was entered correctly
+    function isValidCreditCard() {
+        // // check credit card expiration month - not required
+        // const ccMonthSelect = document.getElementById('exp-month');
+        // const selectedMonthIndex = ccMonthSelect.selectedIndex;
+        // const ccMonth = ccMonthSelect.children[selectedMonthIndex].value;
+        // const isValidccMonth = !isNaN(ccMonth);
+
+        // // check credit card expiration year - not required
+        // const ccYearSelect = document.getElementById('exp-year');
+        // const selectedYearIndex = ccYearSelect.selectedIndex;
+        // const ccYear = ccYearSelect.children[selectedYearIndex].value;
+        // const isValidccYear = !isNaN(ccYear);
+
+        return (
+        // isValidccMonth &&
+        // isValidccYear &&
+
+        // a creditcard nr should be between 13 and 16 characters long    
+        /^\d{13,16}$/.test(document.getElementById('cc-num').value) &&
+
+        // a zip code should be 5 digits long
+        /^\d{5}$/.test(document.getElementById('zip').value) &&
+
+        // a creditcard cvv code should be 3 digits long
+        /^\d{3}$/.test(document.getElementById('cvv').value) 
+        );
+    }
+
+    // console.log('--------------------------------------------------');
+    // console.log('isValidName: ' + isValidName());
+    // console.log('isValidEmail: ' + isValidEmail());
+    // console.log('AreActivitiesSelected: ' + AreActivitiesSelected());
+
+    let isReadyForSubmission = 
+        isValidName() &&
+        isValidEmail() &&
+        AreActivitiesSelected();
+
+    if(getPaymentMethodId() === 'credit-card') {
+        console.log('The credit card payment option was selected');
+        isReadyForSubmission = isReadyForSubmission && isValidCreditCard();
+    } else {
+        console.log('The credit card payment option was NOT selected');
+    }
+    console.log('isReadyForSubmission = ' + isReadyForSubmission);
+
+    // The user did not enter all information correctly, prevent submission
+    if(!isReadyForSubmission) {
+        event.preventDefault();
+    }
+
+    /*
+    Note:
+        Only validate the three credit card fields if "credit card" is the 
+        selected payment option.
+        Only call `preventDefault` on the `event` object if one or more of
+        the required fields is invalid.
+    */
+});
+
